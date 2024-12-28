e8b3c08af6f485f6c9037159dfeadc2e53b4b833
Philipp Stephan <p.stephan@mediaire.de>
2024-12-28 01:21:42 +0100

    qs1: fov

--- scenes/qs1.glsl
+++ scenes/qs1.glsl
@@ -1,4 +1,4 @@
-/// //
+/// // 1
 /// uniform int max_iterations;  // =80 [1,200,1]
 /// uniform bool dbg_normal;  // =False
 /// uniform float rot_y_speed;  // =6.619999885559082 [-10,10]
@@ -7,6 +7,7 @@
 /// uniform float pulse_amp;  // =0.00800000037997961 [0,0.1]
 /// uniform float pulse_freq;  // =0.7400000095367432 [0,1]
 /// uniform vec3 color_qs1;  // <color> =(1.0,0.351190447807312,0.0) [0.0,1.0,0.01]
+/// uniform float fov;  // =-3.378000020980835 [-5.0,0.0]
 
 /* TODO
  * fov
@@ -36,6 +37,8 @@ uniform float qs1_width; // =0.1 [0, 1]
 uniform float pulse_amp; // =0.05 [0, 0.1]
 uniform float pulse_freq; // =0.1 [0, 1]
 uniform vec3 color_qs1; // <color> =(1.,1.,1.)
+
+uniform float fov; // [0.,5.]
 
 struct Material {
     vec3 color;
@@ -143,10 +146,89 @@ vec3 warp_speed(vec2 uv) {
     return clamp(col, 0.0, 1.0);
 }
 
+float colormap_red(float x) {
+    if (x < 0.0) {
+        return 54.0 / 255.0;
+    } else if (x < 20049.0 / 82979.0) {
+        return (829.79 * x + 54.51) / 255.0;
+    } else {
+        return 1.0;
+    }
+}
+
+float colormap_green(float x) {
+    if (x < 20049.0 / 82979.0) {
+        return 0.0;
+    } else if (x < 327013.0 / 810990.0) {
+        return (8546482679670.0 / 10875673217.0 * x - 2064961390770.0 / 10875673217.0) / 255.0;
+    } else if (x <= 1.0) {
+        return (103806720.0 / 483977.0 * x + 19607415.0 / 483977.0) / 255.0;
+    } else {
+        return 1.0;
+    }
+}
+
+float colormap_blue(float x) {
+    if (x < 0.0) {
+        return 54.0 / 255.0;
+    } else if (x < 7249.0 / 82979.0) {
+        return (829.79 * x + 54.51) / 255.0;
+    } else if (x < 20049.0 / 82979.0) {
+        return 127.0 / 255.0;
+    } else if (x < 327013.0 / 810990.0) {
+        return (792.02249341361393720147485376583 * x - 64.364790735602331034989206222672) / 255.0;
+    } else {
+        return 1.0;
+    }
+}
+
+vec4 colormap(float x) {
+    return vec4(colormap_red(x), colormap_green(x), colormap_blue(x), 1.0);
+}
+
+float rand(vec2 n) {
+    return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+}
+
+float noise(vec2 p) {
+    vec2 ip = floor(p);
+    vec2 u = fract(p);
+    u = u * u * (3.0 - 2.0 * u);
+
+    float res = mix(
+            mix(rand(ip), rand(ip + vec2(1.0, 0.0)), u.x),
+            mix(rand(ip + vec2(0.0, 1.0)), rand(ip + vec2(1.0, 1.0)), u.x), u.y);
+    return res * res;
+}
+
+const mat2 mtx = mat2(0.80, 0.60, -0.60, 0.80);
+
+float fbm(vec2 p) {
+    float f = 0.0;
+
+    f += 0.500000 * noise(p + u_Time);
+    p = mtx * p * 2.02;
+    f += 0.031250 * noise(p);
+    p = mtx * p * 2.01;
+    f += 0.250000 * noise(p);
+    p = mtx * p * 2.03;
+    f += 0.125000 * noise(p);
+    p = mtx * p * 2.01;
+    f += 0.062500 * noise(p);
+    p = mtx * p * 2.04;
+    f += 0.015625 * noise(p + sin(u_Time));
+
+    return f / 0.96875;
+}
+
+float pattern(in vec2 p) {
+    return fbm(p + fbm(p + fbm(p)));
+}
+
 void main() {
     vec2 uv = (gl_FragCoord.xy * 2. - u_Resolution.xy) / u_Resolution.y;
 
-    vec3 origin = vec3(0, 0, -3);
+    vec3 origin = vec3(0, 0, fov);
     vec3 dir = normalize(vec3(uv, 1));
     vec3 color;
     float travel = 0.;
