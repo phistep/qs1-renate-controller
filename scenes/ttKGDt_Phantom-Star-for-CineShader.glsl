a88214720ef60733068bd354930244b5284033db
Philipp Stephan <p.stephan@mediaire.de>
2024-12-30 13:56:49 +0100

    phantom-star: uniforms

--- scenes/ttKGDt_Phantom-Star-for-CineShader.glsl
+++ scenes/ttKGDt_Phantom-Star-for-CineShader.glsl
@@ -25,56 +25,65 @@
 #define iDate vec4(1970.0, 1.0, 1.0, 0.0)
 #define iSampleRate 44100.0
 
-
 precision highp float;
 
+uniform float camera_rot_speed; // =1. [0.,6.]
+uniform float camera_pos_speed; // =3. [0.,25.]
+
+uniform vec3 box_color; // <color> =(0.01,0.011,0.012) [0.,0.25,0.01]
+uniform vec3 box_dim; // =(0.4,0.8,0.3)
+uniform float box_rot_xy; // =0.3
+uniform float box_rot_xz; // =0.1
+uniform vec3 box_offset; // =(5.,5.,16.) [0.,20.]
+
+uniform vec3 flash_color; // <color> =(0.,.002,.005) [0.,.01,.001]
+uniform float flash_intensity; // =3. [0.,10.]
+uniform float flash_speed; // =24. [0.,50.]
 
 mat2 rot(float a) {
     float c = cos(a), s = sin(a);
-    return mat2(c,s,-s,c);
+    return mat2(c, s, -s, c);
 }
 
 const float pi = acos(-1.0);
-const float pi2 = pi*2.0;
+const float pi2 = pi * 2.0;
 
 vec2 pmod(vec2 p, float r) {
-    float a = atan(p.x, p.y) + pi/r;
+    float a = atan(p.x, p.y) + pi / r;
     float n = pi2 / r;
-    a = floor(a/n)*n;
-    return p*rot(-a);
+    a = floor(a / n) * n;
+    return p * rot(-a);
 }
 
-float box( vec3 p, vec3 b ) {
+float box(vec3 p, vec3 b) {
     vec3 d = abs(p) - b;
-    return min(max(d.x,max(d.y,d.z)),0.0) + length(max(d,0.0));
+    return min(max(d.x, max(d.y, d.z)), 0.0) + length(max(d, 0.0));
 }
 
 float ifsBox(vec3 p) {
-    for (int i=0; i<5; i++) {
+    for (int i = 0; i < 5; i++) {
         p = abs(p) - 1.0;
-        p.xy *= rot(iTime*0.3);
-        p.xz *= rot(iTime*0.1);
+        p.xy *= rot(iTime * box_rot_xy);
+        p.xz *= rot(iTime * box_rot_xz);
     }
     p.xz *= rot(iTime);
-    return box(p, vec3(0.4,0.8,0.3));
+    return box(p, box_dim);
 }
 
 float map(vec3 p, vec3 cPos) {
     vec3 p1 = p;
-    p1.x = mod(p1.x-5., 10.) - 5.;
-    p1.y = mod(p1.y-5., 10.) - 5.;
-    p1.z = mod(p1.z, 16.)-8.;
+    p1 = mod(p1 - box_offset / 2., box_offset) - box_offset / 2.;
     p1.xy = pmod(p1.xy, 5.0);
     return ifsBox(p1);
 }
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 p = (fragCoord.xy * 2.0 - iResolution.xy) / min(iResolution.x, iResolution.y);
 
-    vec3 cPos = vec3(0.0,0.0, -3.0 * iTime);
+    vec3 cPos = vec3(0.0, 0.0, -camera_pos_speed * iTime);
     // vec3 cPos = vec3(0.3*sin(iTime*0.8), 0.4*cos(iTime*0.3), -6.0 * iTime);
     vec3 cDir = normalize(vec3(0.0, 0.0, -1.0));
-    vec3 cUp  = vec3(sin(iTime), 1.0, 0.0);
+    vec3 cUp = vec3(sin(iTime) * camera_rot_speed, 1.0, 0.0);
     vec3 cSide = cross(cDir, cUp);
 
     vec3 ray = normalize(cSide * p.x + cUp * p.y + cDir);
@@ -87,8 +96,8 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
         vec3 pos = cPos + ray * t;
         float dist = map(pos, cPos);
         dist = max(abs(dist), 0.02);
-        float a = exp(-dist*3.0);
-        if (mod(length(pos)+24.0*iTime, 30.0) < 3.0) {
+        float a = exp(-dist * flash_intensity);
+        if (mod(length(pos) + flash_speed * iTime, 30.0) < 3.0) {
             a *= 2.0;
             acc2 += a;
         }
@@ -96,7 +105,8 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
         t += dist * 0.5;
     }
 
-    vec3 col = vec3(acc * 0.01, acc * 0.011 + acc2*0.002, acc * 0.012+ acc2*0.005);
+    vec3 col = acc * box_color;
+    col += acc2 * flash_color;
     fragColor = vec4(col, 1.0 - t * 0.03);
 }
 
