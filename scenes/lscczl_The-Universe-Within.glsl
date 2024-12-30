b84dbbda78d5c004e4eb3e697188a83d69dd018f
Philipp Stephan <p.stephan@mediaire.de>
2024-12-30 12:57:10 +0100

    universe: uniforms

--- scenes/lscczl_The-Universe-Within.glsl
+++ scenes/lscczl_The-Universe-Within.glsl
@@ -51,9 +51,34 @@
 // https://youtu.be/3CycKKJiwis
 
 #define S(a, b, t) smoothstep(a, b, t)
-#define NUM_LAYERS 4.
 
-//#define SIMPLE
+// TODO no full black
+
+uniform bool simple; // =False
+uniform float glow; // =0.5 [0.,1.]
+float fft = glow; //u_Microphone[2];
+uniform float speed; // =0.1 [0.,2.]
+
+uniform bool color_auto; // =True
+uniform vec3 color; // <color> =(1.,1.,1.)
+
+uniform float layer_count; // =4. [1.,10.]
+uniform float layer_size_min; // =1. [1.,50.]
+uniform float layer_size_max; // =15. [1.,50.]
+uniform float layer_fade_min; // =0. [0.,1.]
+uniform float layer_fade_max; // =6. [0.,1.]
+
+uniform float displacement; // =.4 [0.,1.]
+
+uniform float line_fade_min; // =0.5 [0.,5.]
+uniform float line_fade_max; // =1.5 [0.,5.]
+uniform float line_r1; // =0.04 [0.,.1]
+uniform float line_r2; // =0.01 [0.,.1]
+
+uniform float sparkle_intensity; // =.005 [0.,0.1,.001]
+uniform float sparkle_damping; // =20. [0., 100.]
+uniform float sparkle_pulse_amp; // =.4 [0.,1.]
+uniform float sparkle_pulse_freq; // =.5 [0.,25.]
 
 float N21(vec2 p) {
     vec3 a = fract(vec3(p.xyx) * vec3(213.897, 653.453, 253.098));
@@ -66,7 +91,7 @@ vec2 GetPos(vec2 id, vec2 offs, float t) {
     float n1 = fract(n * 10.);
     float n2 = fract(n * 100.);
     float a = t + n;
-    return offs + vec2(sin(a * n1), cos(a * n2)) * .4;
+    return offs + vec2(sin(a * n1), cos(a * n2)) * displacement;
 }
 
 float GetT(vec2 ro, vec2 rd, vec2 p) {
@@ -85,12 +110,12 @@ float df_line(in vec2 a, in vec2 b, in vec2 p)
 }
 
 float line(vec2 a, vec2 b, vec2 uv) {
-    float r1 = .04;
-    float r2 = .01;
+    float r1 = line_r1;
+    float r2 = line_r2;
 
     float d = df_line(a, b, uv);
     float d2 = length(a - b);
-    float fade = S(1.5, .5, d2);
+    float fade = S(line_fade_max, line_fade_min, d2);
 
     fade += S(.05, .02, abs(d2 - .75));
     return S(r1, r2, d) * fade;
@@ -117,10 +142,10 @@ float NetLayer(vec2 st, float n, float t) {
 
         float d = length(st - p[i]);
 
-        float s = (.005 / (d * d));
+        float s = (sparkle_intensity / (d * d));
         s *= S(1., .7, d);
-        float pulse = sin((fract(p[i].x) + fract(p[i].y) + t) * 5.) * .4 + .6;
-        pulse = pow(pulse, 20.);
+        float pulse = sin((fract(p[i].x) + fract(p[i].y) + t) * sparkle_pulse_freq) * sparkle_pulse_amp + (1. - sparkle_pulse_amp);
+        pulse = pow(pulse, sparkle_damping);
 
         s *= pulse;
         sparkle += s;
@@ -143,7 +168,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     vec2 uv = (fragCoord - iResolution.xy * .5) / iResolution.y;
     vec2 M = iMouse.xy / iResolution.xy - .5;
 
-    float t = iTime * .1;
+    float t = iTime * speed;
 
     float s = sin(t);
     float c = cos(t);
@@ -152,31 +177,33 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     M *= rot * 2.;
 
     float m = 0.;
-    for (float i = 0.; i < 1.; i += 1. / NUM_LAYERS) {
+    for (float i = 0.; i < 1.; i += 1. / layer_count) {
         float z = fract(t + i);
-        float size = mix(15., 1., z);
-        float fade = S(0., .6, z) * S(1., .8, z);
+        float size = mix(layer_size_max, layer_size_min, z);
+        float fade = S(layer_fade_min, layer_fade_max, z) * S(1., .8, z);
 
         m += fade * NetLayer(st * size - M * z, i, iTime);
     }
 
-    float fft = u_Microphone[2];
     float glow = -uv.y * fft * 2.;
 
-    vec3 baseCol = vec3(s, cos(t * .4), -sin(t * .24)) * .4 + .6;
+    vec3 baseCol = color;
+    if (color_auto) {
+        baseCol = vec3(s, cos(t * .4), -sin(t * .24)) * .4 + .6;
+    }
     vec3 col = baseCol * m;
     col += baseCol * glow;
 
-    #ifdef SIMPLE
-    uv *= 10.;
-    col = vec3(1) * NetLayer(uv, 0., iTime);
-    uv = fract(uv);
-    //if(uv.x>.98 || uv.y>.98) col += 1.;
-    #else
-    col *= 1. - dot(uv, uv);
-    t = mod(iTime, 230.);
-    col *= S(0., 20., t) * S(224., 200., t);
-    #endif
+    if (simple) {
+        uv *= 10.;
+        col = vec3(1) * NetLayer(uv, 0., iTime);
+        uv = fract(uv);
+        //if(uv.x>.98 || uv.y>.98) col += 1.;
+    } else {
+        col *= 1. - dot(uv, uv);
+        t = mod(iTime, 30.);
+        col *= S(0., 20., t) * S(224., 200., t);
+    }
 
     fragColor = vec4(col, 1);
 }
